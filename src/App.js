a76629fd95a22652037923ee3038276b785c64af
Iggy420 <igneriilla@gmail.com>
2020-03-29 17:02:11 -0500

    search bar and button. logic for the axios routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import axios from "axios";
+import React, { useState } from "react";
 
 function App() {
+  const [book, setBook] = useState("");
+  const [result, setResult] = useState([]);
+  const [apiKey, seApiKey] = useState(
+    "AIzaSyDOzrw_nvJ55U2kquaL60Zye-S9LTPCplU"
+  );
+
+  function handleChange(event) {
+    const book = event.target.value;
+    setBook(book);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    axios
+      .get(
+        "GET https://www.googleapis.com/books/v1/volumes?q=" +
+          book +
+          "&key=" +
+          apiKey +
+          "&maxResults=40"
+      )
+      .then(data => {
+        console.log(data);
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <h1>Book Search App</h1>
+      <form onSubmit={handleSubmit}>
+        <div class="form-group">
+          <input
+            type="text"
+            onChange={handleChange}
+            className="form-control mt-10"
+            placeholder="Search from Books"
+            autoComplete="off"
+          />
+          <button className="btn btn-danger">Search</button>
+        </div>
+      </form>
     </div>
   );
 }
